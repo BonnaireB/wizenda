aab26c158c81a15b3f6e4a72c6b8154745053552
BonnaireB <bonnaire.benjamin@gmail.com>
2019-03-19 14:24:38 -0400

    ajout classe Agenda et evenement

--- objets.py
+++ objets.py
@@ -6,7 +6,13 @@ class Utilisateur:
         self.mdp = mdp
 
 class Evenement:
-    def __init__(self, nom, email, mdp):
-        self.nom = nom
-        self.email = email
-        self.mdp = mdp
+    def __init__(self, description, heure_debut, heure_fin, recurrent, color):
+        self.description = description
+        self.heure_debut = heure_debut
+        self.heure_fin = heure_fin
+        self.recurrent = recurrent
+        self.color = color
+
+class Agenda:
+    def __init__(self, email, evenements):
+        pass
