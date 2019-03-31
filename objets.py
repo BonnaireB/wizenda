557c17b025b21c8bf7dde72d3e13c0549242c7cc
fiyam <fiyam86@gmail.com>
2019-03-31 13:04:44 -0400

    removing method

--- objets.py
+++ objets.py
@@ -22,26 +22,26 @@ class Utilisateur:
  #      self.color = None
 
 
-    def modifier_DescriptionEvenement(self,nouveau_description):
-        self.description = nouveau_description 
+#   def modifier_DescriptionEvenement(self,nouveau_description):
+#       self.description = nouveau_description 
 
-    def modifier_heure_debut(self, heure_debut):
-        self.heure_debut = heure_debut 
+#    def modifier_heure_debut(self, heure_debut):
+#        self.heure_debut = heure_debut 
 
-    def modifier_heure_fin(self, heure_fin):
-        self.heure_fin = heure_fin 
+#    def modifier_heure_fin(self, heure_fin):
+#        self.heure_fin = heure_fin 
 
-    def modifier_recurrent(self, recurrent):
-        self.recurrent = recurrent
+#    def modifier_recurrent(self, recurrent):
+#        self.recurrent = recurrent
         
-    def modifier_color(self, color):
-        self.color = color
+#    def modifier_color(self, color):
+#        self.color = color
         
 
-class Agenda:
-    def __init__(self, email):
-        self.email = email
-        self.events = None
+#class Agenda:
+#    def __init__(self, email):
+#        self.email = email
+#        self.events = None
         
-    def add_event(self, evenement):
-        self.events.append(evenement)
+#    def add_event(self, evenement):
+#        self.events.append(evenement)
